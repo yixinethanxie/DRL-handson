3c8bfd3ef7c4dd1e8c26fb10a772fdc892c88c2f
Yixin Xie <yixinethanxie@gmail.com>
2018-11-07 23:40:01 -0600

    **DEV**: can't import modules from lib after adding __init.py

--- DQNPong/training.py
+++ DQNPong/training.py
@@ -1,4 +1,7 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+import sys
+if "lib/" not in sys.path:
+    sys.path.append("lib/")
 
 from wrappers import make_env
 from dqn_models import DQN
@@ -35,33 +38,32 @@ Experience=collections.namedtuple("Experience",field_names=["state","action","re
 class ExperienceBuffer:
     def __init__(self,capacity):
         self.buffer=collections.deque(maxlen=capacity)
-        
+
     def __len__(self):
         return len(self.buffer)
-    
+
     def append(self,experience):
         self.buffer.append(experience)
-    
+
     def sample(self,batch_size):
         indices=np.random.choice(len(self.buffer),batch_size,replace=False)
         states,actions,rewards,dones,next_states=zip(*[self.buffer[idx] for idx in indices])
         return np.array(states),np.array(actions),np.array(rewards,dtype=np.float32), \
                 np.array(dones,dtype=np.uint8),np.array(next_states)
-    
-    
+
 class Agent:
     def __init__(self,env,exp_buffer):
         self.env=env
         self.exp_buffer=exp_buffer
         self._reset()
-        
+
     def _reset(self):
         self.state=env.reset()
         self.total_reward=0.
-        
+
     def play_step(self,net,epsilon=0.,device="cpu"):
         done_reward=None
-        
+
         if np.random.random() < epsilon:
             action=env.action_space.sample()
         else:
@@ -70,11 +72,8 @@ class Agent:
             q_vals_v=net(state_v)
             _,act_v=torch.max(q_vals_v,dim=1)
             action=int(act_v.item())
-        
-        
         new_state,reward,is_done,_=self.env.step(action)
         self.total_reward+=reward
-        
         exp=Experience(self.state,action,reward,is_done,new_state)
         self.exp_buffer.append(exp)
         self.state=new_state
@@ -82,21 +81,18 @@ class Agent:
             done_reward=self.total_reward
             self._reset()
         return done_reward
-    
+
 def calc_loss(batch, net,tgt_net,device="cpu"):
     states,actions,rewards,dones,next_states=batch
-    
     states_v=torch.tensor(states).to(device)
     next_states_v=torch.tensor(next_states).to(device)
     actions_v=torch.tensor(actions).to(device)
     rewards_v=torch.tensor(rewards).to(device)
     done_mask=torch.ByteTensor(dones).to(device)
-    
     state_action_values=net(states_v).gather(1,actions_v.unsqueeze(-1)).squeeze(-1)
     next_state_values=tgt_net(next_states_v).max(1)[0]
     next_state_values[done_mask]=0.
     next_state_values=next_state_values.detach()
-    
     expected_state_action_values=next_state_values*GAMMA+rewards_v
     return nn.MSELoss()(state_action_values,expected_state_action_values)
 
@@ -113,11 +109,9 @@ if __name__=="__main__":
     tgt_net=DQN(env.observation_space.shape,env.action_space.n).to(device)
     writer=SummaryWriter(comment=f"-{args.env}")
     print(net)
-    
     buffer=ExperienceBuffer(REPLAY_SIZE)
     agent=Agent(env,buffer)
     epsilon=EPSILON_START
-    
     optimizer=optim.Adam(net.parameters(), lr=LEARNING_RATE)
     total_rewards=[]
     frame_idx=0
@@ -128,7 +122,6 @@ if __name__=="__main__":
     while True:
         frame_idx+=1
         epsilon=max(EPSILON_FINAL,EPSILON_START-frame_idx/EPSILON_DECAY_LAST_FRAME)
-        
         reward=agent.play_step(net,epsilon,device=device)
         if reward is not None:
             total_rewards.append(reward)
@@ -137,7 +130,6 @@ if __name__=="__main__":
             ts=time.time()
             mean_reward=np.mean(total_rewards[-100:])
             print(f"{frame_idx}: done {len(total_rewards)} games, eps {round(epsilon,2)}, speed {round(speed,2)} f/s ")
-            
             writer.add_scalar("epsilon",epsilon,frame_idx)
             writer.add_scalar("speed", speed, frame_idx)
             writer.add_scalar("reward_100", mean_reward, frame_idx)
@@ -151,13 +143,10 @@ if __name__=="__main__":
             if mean_reward>args.reward:
                 print(f"Solved in {frame_idx} frames")
                 break
-        
         if len(buffer)<REPLAY_START_SIZE:
             continue
-        
         if frame_idx%SYNC_TARGET_FRAMES==0:
             tgt_net.load_state_dict(net.state_dict())
-        
         optimizer.zero_grad()
         batch=buffer.sample(BATCH_SIZE)
         loss_t=calc_loss(batch,net,tgt_net,device=device)
